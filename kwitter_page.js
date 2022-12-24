533ebf604f81e8d610162c751c650a2b6db04219
SportsStar <107591657+SportsStar@users.noreply.github.com>
2022-12-24 10:05:58 -0500

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -31,7 +31,16 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+console.log(firebase_message_id);
+console.log(message_data);
+name = message_data['name'];
+message = message_data['message'];
+like = message_data['like'];
+name_with_tag = "<h4>" + name + "</h4>";
+message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>Likes :"+ like +"</button>";
+row = name_with_tag + message_with_tag + like_button;
+document.getElementById("output").innerHTML += row;
 //End code
       } });  }); }
 getData();
@@ -39,4 +48,15 @@ function logout() {
       localStorage.removeItem("user_name");
       localStorage.removeItem("room_name");
       window.location = "index.html";
+}
+function updateLike(message_id)
+{
+      button_id = message_id;
+      likes = document.getElementById(button_id).value;
+      updated_likes = Number(likes) + 1;
+      console.log(updated_likes);
+      firebase.database().ref(room_name).child(message_id).update({
+       like: updated_likes
+
+      });
 }
